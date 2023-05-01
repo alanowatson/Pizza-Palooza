8cf811fe0500690caca3255471a3475c99df6aca
Alan Watson <alanowatson@gmail.com>
2023-05-01 14:52:49 -0700

    cleanup: Initial jest skeleton and route catch all

--- app/javascript/components/__tests__/Pizzas.test.js
+++ app/javascript/components/__tests__/Pizzas.test.js
@@ -2,8 +2,44 @@
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import Pizzas from '../Pizzas';
+// import Pizzas from '../Pizzas';
 
-test('renders Pizzas management', () => {
-  // ...
+describe('Pizza Management', () => {
+  // Test: See a list of available toppings
+  test('displays a list of available pizzas', () => {
+    // Render the Toppings component
+    // Pass in initial toppings as a prop if needed
+    // Check if the list is displayed with the correct toppings
+  });
+
+  // Test: Add a new topping
+  test('adds a new pizzas', () => {
+    // Render the Toppings component
+    // Simulate user input for a new topping
+    // Click on the "Add Topping" button
+    // Check if the new topping has been added to the list
+  });
+
+  // Test: Delete an existing topping
+  test('deletes an existing pizzas', () => {
+    // Render the Toppings component
+    // Simulate user click on the "Delete" button for a specific topping
+    // Check if the topping has been removed from the list
+  });
+
+  // Test: Update an existing topping
+  test('updates an existing pizzas', () => {
+    // Render the Toppings component
+    // Simulate user click on the "Edit" button for a specific topping
+    // Change the topping name and save
+    // Check if the topping has been updated in the list
+  });
+
+  // Test: Do not allow duplicate toppings
+  test('does not allow duplicate Pizza', () => {
+    // Render the Toppings component
+    // Simulate user input for an existing topping
+    // Click on the "Add Topping" button
+    // Check if an error message is displayed or the topping is not added to the list
+  });
 });
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   root to: 'home#index'
-  # Other routes
-end
 
-Rails.application.routes.draw do
   resources :users, only: [:create]
   post 'login', to: 'authentication#login'
+
   # Other routes for pizzas and toppings
+
+  match '*path', to: 'home#index', via: :all
 end
